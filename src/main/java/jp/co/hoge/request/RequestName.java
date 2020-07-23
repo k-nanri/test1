22b7cd2c3b32e8f051315d50d052bf000580922f
k-nanri <kotaro.nanri15@gmail.com>
2020-07-23 10:53:06 +0900

    commit

---
+++ src/main/java/jp/co/hoge/request/RequestName.java
@@ -0,0 +1,14 @@
+package jp.co.hoge.request;
+
+public class RequestName {
+	
+	private final String name;
+	
+	public RequestName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+
+}
